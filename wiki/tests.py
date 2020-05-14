bac3cb79a91c189c453001eddf6cac1a3a366b5b
ninjaaung <ninja.aung02@gmail.com>
2020-05-14 00:08:02 -0700

    test_create_page

--- wiki/tests.py
+++ wiki/tests.py
@@ -9,10 +9,30 @@ class WikiTests(TestCase):
         test_user = User.objects.create_user(
             username='test', 
             password='test'
-            )
+         )
 
         self.client.login(
             username='test', 
             password='test'
         )
-        print(self.request.user.username)
+        print(test_user.id)
+        self.assertEqual(test_user.username,'test')
+        
+
+def test_create_page(self):
+    test_user = User.objects.create()
+    # Post data to be sent via the form
+    post_data = {
+        'title': 'asdasdasdasd',
+        'content': 'asdasdasdasdasdasd',
+        'author': test_user.id
+    }
+
+    res = self.client.post('/create/', data=post_data)
+
+    self.assertEqual(res.status_code, 302)
+
+    page_object = Page.objects.get(title='asdasdasdasd')
+
+    self.assertEqual(page_object.title, 'asdasdasdasdasdasd')
+
