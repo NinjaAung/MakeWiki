2c914871563a61b7049e2aec039bfcfd6da0a578
ninjaaung <ninja.aung02@gmail.com>
2020-05-14 00:43:00 -0700

    Fixed indentation and add test edit no author

--- wiki/tests.py
+++ wiki/tests.py
@@ -19,30 +19,37 @@ class WikiTests(TestCase):
         self.assertEqual(test_user.username,'test')
         
 
-def test_create_page(self):
-    test_user = User.objects.create()
-    # Post data to be sent via the form
-    post_data = {
-        'title': 'asdasdasdasd',
-        'content': 'asdasdasdasdasdasd',
-        'author': test_user.id
-    }
-
-    res = self.client.post('/create/', data=post_data)
-    page_object = Page.objects.get(title='asdasdasdasd')
-
-    self.assertEqual(res.status_code, 302)
-    self.assertEqual(page_object.title, 'asdasdasdasd')
-    self.assertEqual(page_object.content, 'asdasdasdasdasdasd')
-
-def test_edit_page(self):
+    def test_create_page(self):
         test_user = User.objects.create()
+        # Post data to be sent via the form
+        post_data = {
+            'title': 'asdasdasdasd',
+            'content': 'asdasdasdasdasdasd',
+            'author': test_user.id
+        }
 
+        res = self.client.post('/create/', data=post_data)
+        page_object = Page.objects.get(title='asdasdasdasd')
+
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(page_object.title, 'asdasdasdasd')
+        self.assertEqual(page_object.content, 'asdasdasdasdasdasd')
+
+    def test_edit_page(self):
+        test_user = User.objects.create_user(
+            username='test', 
+            password='test'
+        )
+
+        self.client.login(
+            username='test', 
+            password='test'
+        )
 
         page = Page.objects.create(
             title='asdasdasdasd', 
             content='asdasdasdasdasdasd', 
-            author=test_user.id
+            author=test_user
             )
 
         page.save() 
@@ -50,32 +57,31 @@ def test_edit_page(self):
         post_data = {
             'title': 'zxczxczxczxczxczxczxc',
             'content': 'zxczxczxczxczxczxczxczxc',
-            'author': test_user.id
+            'author': test_user
         }
 
-        res = self.client.post('/{}/'.format(slugify(page.title)), post_data)
-        self.assertEqual(res.status_code, 302)
+        self.assertEqual(self.client.post('/{}/'.format(slugify(page.title)), post_data).status_code, 302)
 
-def test_edit_page_no_author(self):
-    test_user = User.objects.create()
+    def test_edit_page_no_author(self):
+        test_user = User.objects.create_user(
+            username='test', 
+            password='test'
+        )
 
+        self.client.login(
+            username='test', 
+            password='test'
+        )
 
-    page = Page.objects.create(
-        title='asdasdasdasd', 
-        content='asdasdasdasdasdasd', 
-        author='ssdfsdfsdfsd',
-    )
+        page = Page.objects.create(
+            title='asdasdasdasd', 
+            content='asdasdasdasdasdasd', 
+            author=test_user
+        )
 
-    page.save() 
-
-    post_data = {
-        'title': 'zxczxczxczxczxczxczxc',
-        'content': 'zxczxczxczxczxczxczxczxc',
-        'author': test_user.id
-    }
-
-    res = self.client.post('/{}/'.format(slugify(page.title)), post_data)
-    self.assertEqual(res.status_code != 302)
+        page.save()
+ 
+        self.client.logout()
 
     
 
