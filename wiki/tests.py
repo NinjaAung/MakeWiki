a6ac2d348d588ed248d03eae9ebe23d9cd34b74a
ninjaaung <ninja.aung02@gmail.com>
2020-05-13 23:59:03 -0700

    signup test

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 from django.test import TestCase
+from wiki.models import Page
 
-# Create your tests here.
+
+class WikiTests(TestCase):
+    def test_signup(self):
+        test_user = User.objects.create_user(
+            username='test', 
+            password='test'
+            )
+
+        self.client.login(
+            username='test', 
+            password='test'
+        )
+        print(self.request.user.username)
