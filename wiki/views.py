846ceb6394a3d4ea0ce6a46d118a8d45abb21bb9
ninjaaung <ninja.aung02@gmail.com>
2020-05-13 23:46:21 -0700

    Clean | TEsts

--- wiki/views.py
+++ wiki/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.text import slugify
 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from wiki.models import Page
 from wiki.forms import PageForm
@@ -42,17 +42,20 @@ class PageDetailView(DetailView):
             'form': PageForm()
         })
 
-    # Edit form
     def post(self, req, slug):
         '''Edit the page's information'''
         form = PageForm(req.POST)
 
         page = self.get_queryset().get(slug__iexact=slug)
+
+        '''['title', 'author', 'content'] '''
         page.title = req.POST['title']
         page.content = req.POST['content']
-        page.author = req.user     
+        page.author = req.user
+        '''['title', 'author', 'content'] '''
+     
         page.slug = slugify(page.title)
+
         page.save()
 
-        
         return HttpResponseRedirect(reverse('wiki-details-page', args=[page.slug]))
